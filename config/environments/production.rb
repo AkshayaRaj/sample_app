21fdfffbc35acfc6e7a34c0dcfb81439e3b42803
Akshay Raj Dayal <ard.astro@gmail.com>
2016-04-20 23:04:41 +0530

    fix hostname to rrctalk.herokuapp.com in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'librex-app.herokuapp.com'
+  host = 'rrctalk.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
